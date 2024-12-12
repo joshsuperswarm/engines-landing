6b6f8b34e10b67ddc1eec9bba857a780bc366db4
Josh Valdez <exxonvaldeez@gmail.com>
2024-12-11 19:22:05 -0800

    Fix top page margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+        <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0">
           {children}
           <Analytics />
           <SpeedInsights />
